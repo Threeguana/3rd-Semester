302823e0edb4267b3c313e8e0584f4bdbcf198a6
Andini Tribuana <threebuanandini@gmail.com>
2025-12-01 22:19:17 +0700

    recap

--- Object Oriented Programming/Quiz1/Borrow.java
+++ Object Oriented Programming/Quiz1/Borrow.java
@@ -1,29 +1,31 @@
 public class Borrow {
-    private UniStudent2 UniStudent;
+    private UniStudent2 uniStudent;
     private Book borrowedBook;
 
-    public Borrow(UniStudent2 UniStudent) {
-        this.UniStudent = UniStudent;
+    public Borrow(UniStudent2 uniStudent) {
+        this.uniStudent = uniStudent;
     }
 
-    public void borrowedBook(Book b) {
-        this.borrowedBook = b;
-        System.out.println(UniStudent.getName() + " borrow book: " + b.getTitle());
+    public void borrowBook(Book book) {
+        this.borrowedBook = book;
+        System.out.println(uniStudent.getName() + " borrowed the book: " + book.getTitle());
     }
 
-    public void returnBook(Book b) {
-        if (borrowedBook != null && borrowedBook == b) {
-            System.out.println(UniStudent.getName() + " return book: " + b.getTitle());
+    public void returnBook(Book book) {
+        if (borrowedBook != null && borrowedBook == book) {
+            System.out.println(uniStudent.getName() + " returned the book: " + book.getTitle());
             borrowedBook = null;
+        } else {
+            System.out.println("The student did not borrow this book.");
         }
     }
 
-    public void showBr() {
+    public void showBorrowedBook() {
         if (borrowedBook != null) {
-            System.out.println(
-                    "Mahasiswa " + getName() + " mengembalikan buku " + borrowedBook.getTitle());
+            System.out.println("Student " + uniStudent.getName() + " is currently borrowing: "
+                    + borrowedBook.getTitle());
         } else {
-            System.out.println("Tidak ada buku yang sedang dipinjam.");
+            System.out.println("No book is currently being borrowed.");
         }
     }
 }
